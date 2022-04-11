b4bd452c7f251c022d7da167cfd9e2f987afdc55
songuu <1101309860@qq.com>
2022-04-11 20:58:08 +0800

    chore: first todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { GlobalStyle } from './styles/GlobalStyle'
-
 import { Greetings } from './components/Greetings'
 
 export function App() {
   return (
     <>
-      <GlobalStyle />
       <Greetings />
     </>
   )
