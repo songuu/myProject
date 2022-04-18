913439301f9ad20d831ad4935b97bcc2260981bb
lala <1101309860@qq.com>
2022-04-18 16:59:39 +0800

    chore: 更新

--- src/components/ContextMenu/index.tsx
+++ src/components/ContextMenu/index.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { forwardRef, useState, useImperativeHandle } from 'react'
 
-interface Iprops {
-  visible: boolean;
+export interface MenuImperativeProps {
+  /** 打开下拉 */
+  openMenu: (e: React.MouseEvent<HTMLDivElement>) => void
+  /** 关闭下拉 */
+  closeMenu: () => void
 }
 
-const ContextMenu: React.FC<Iprops> = ({ visible }) => {
-  return (
-    visible ? <div>
-      ContextMenu
-    </div> : null
-  );
-}
+const ContextMenu: React.FC = forwardRef(({}, ref) => {
+  const [visible, setVisible] = useState<boolean>(false)
 
-export default ContextMenu;
+  const setMenu = (x: number, y: number) => {}
+
+  useImperativeHandle<unknown, MenuImperativeProps>(ref, () => ({
+    openMenu: (e: React.MouseEvent<HTMLDivElement>) => {
+      setVisible(true)
+    },
+    closeMenu: () => {
+      setVisible(false)
+    },
+  }))
+  return <div>ContextMenu</div>
+})
+
+export default ContextMenu
