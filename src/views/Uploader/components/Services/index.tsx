6fd133e7dbdf714429bd1f298b94b2c72512519e
songuu <1101309860@qq.com>
2022-05-24 07:39:47 +0800

    chore: 更新

--- src/views/Uploader/components/Services/index.tsx
+++ src/views/Uploader/components/Services/index.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 import { debounce, isEqual } from 'lodash'
 
+import classnames from 'classnames'
+
 import { AppStore } from '@mytypes/common'
 
-import { Direction } from '@constants/enums'
+import styles from './index.module.less'
 
 enum OssType {
   qiniu,
@@ -26,17 +28,24 @@ interface IProps {
 
 const Services: React.FC<IProps> = ({ activeApp, onAppSwitch }) => {
   const [apps, setApps] = useState<AppStore[]>([])
-  const [page, setPage] = useState<ServicesPage>(ServicesPage.list)
-  const [direction, setDirection] = useState<Direction>(Direction.left)
+  const [page, setPage] = useState<ServicesPage>(ServicesPage.add)
   // 是否为中正在编辑的状态
   const [isEdit, setIsEdit] = useState<boolean>(false)
   // update form 中的数据是否已经被修改
   const [edited, setEdited] = useState<boolean>(false)
   const [loading, setLoading] = useState<boolean>(false)
 
+  const forms: any = useRef({
+    name: '',
+    type: '',
+    AK: '',
+    SK: '',
+  })
+
+  const [inputFocus, setInputFocus] = useState<string>('')
+
   const _toAddPage = () => {
     setPage(ServicesPage.add)
-    setDirection(Direction.right)
   }
   const _toListPage = () => {
     setPage(ServicesPage.list)
@@ -70,9 +79,179 @@ const Services: React.FC<IProps> = ({ activeApp, onAppSwitch }) => {
 
   const renderIcon = (type: OssType) => {}
 
-  const renderSwitch = (param: ServicesPage) => {}
+  const submit = () => {
+    if (Object.keys(forms.current).every((item: any) => !forms.current[item])) {
+      alert('请完善信息')
+      return
+    }
 
-  return <div>Services</div>
+    console.log(213)
+  }
+
+  const renderSwitch = (param: ServicesPage) => {
+    switch (param) {
+      case ServicesPage.list:
+        return apps.length > 0 ? (
+          ''
+        ) : (
+          <section className={styles['apps-main-wrapper']}>
+            <span>没有配置app</span>
+            <button onClick={_toAddPage}>添加</button>
+          </section>
+        )
+      case ServicesPage.add:
+        return (
+          <section className={styles['apps-main-wrapper']}>
+            <span>新增配置</span>
+            <div className={styles['apps-main-wrapper-box']}>
+              <div className={styles['apps-main-wrapper-box-row']}>
+                <div className={styles['apps-main-wrapper-box-col']}>名称:</div>
+                <div className={styles['apps-main-wrapper-box-input']}>
+                  <div
+                    className={classnames(
+                      styles['apps-main-wrapper-box-input-container'],
+                      inputFocus === 'name'
+                        ? styles['apps-main-wrapper-box-input-active']
+                        : ''
+                    )}
+                  >
+                    <div
+                      className={
+                        styles['apps-main-wrapper-box-input-container-input']
+                      }
+                    >
+                      <input
+                        id="name"
+                        onFocus={() => {
+                          setInputFocus('name')
+                        }}
+                        onBlur={() => {
+                          setInputFocus('')
+                        }}
+                        onChange={(e: React.FormEvent<HTMLInputElement>) => {
+                          forms.current.name = e.currentTarget.value
+                        }}
+                        placeholder="请输入名称"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles['apps-main-wrapper-box-row']}>
+                <div className={styles['apps-main-wrapper-box-col']}>类型:</div>
+                <div className={styles['apps-main-wrapper-box-input']}>
+                  <div
+                    className={classnames(
+                      styles['apps-main-wrapper-box-input-container'],
+                      inputFocus === 'type'
+                        ? styles['apps-main-wrapper-box-input-active']
+                        : ''
+                    )}
+                  >
+                    <div
+                      className={
+                        styles['apps-main-wrapper-box-input-container-input']
+                      }
+                    >
+                      <input
+                        id="type"
+                        onFocus={() => {
+                          setInputFocus('type')
+                        }}
+                        onBlur={() => {
+                          setInputFocus('')
+                        }}
+                        onChange={(e: React.FormEvent<HTMLInputElement>) => {
+                          forms.current.type = e.currentTarget.value
+                        }}
+                        placeholder="请输入类型"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles['apps-main-wrapper-box-row']}>
+                <div className={styles['apps-main-wrapper-box-col']}>AK:</div>
+                <div className={styles['apps-main-wrapper-box-input']}>
+                  <div
+                    className={classnames(
+                      styles['apps-main-wrapper-box-input-container'],
+                      inputFocus === 'AK'
+                        ? styles['apps-main-wrapper-box-input-active']
+                        : ''
+                    )}
+                  >
+                    <div
+                      className={
+                        styles['apps-main-wrapper-box-input-container-input']
+                      }
+                    >
+                      <input
+                        id="AK"
+                        onFocus={() => {
+                          setInputFocus('AK')
+                        }}
+                        onBlur={() => {
+                          setInputFocus('')
+                        }}
+                        onChange={(e: React.FormEvent<HTMLInputElement>) => {
+                          forms.current.AK = e.currentTarget.value
+                        }}
+                        placeholder="请输入AK"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles['apps-main-wrapper-box-row']}>
+                <div className={styles['apps-main-wrapper-box-col']}>SK:</div>
+                <div className={styles['apps-main-wrapper-box-input']}>
+                  <div
+                    className={classnames(
+                      styles['apps-main-wrapper-box-input-container'],
+                      inputFocus === 'SK'
+                        ? styles['apps-main-wrapper-box-input-active']
+                        : ''
+                    )}
+                  >
+                    <div
+                      className={
+                        styles['apps-main-wrapper-box-input-container-input']
+                      }
+                    >
+                      <input
+                        id="SK"
+                        onFocus={() => {
+                          setInputFocus('SK')
+                        }}
+                        onBlur={() => {
+                          setInputFocus('')
+                        }}
+                        onChange={(e: React.FormEvent<HTMLInputElement>) => {
+                          forms.current.SK = e.currentTarget.value
+                        }}
+                        placeholder="请输入SK"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button onClick={submit}>确定</button>
+          </section>
+        )
+      default:
+        return ''
+    }
+  }
+
+  return (
+    <div className={styles['services-loading']}>
+      <section className={styles['services-wrapper']}>
+        {renderSwitch(page)}
+      </section>
+    </div>
+  )
 }
 
 export default Services
